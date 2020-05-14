4c5ee24bc1a3d37147d9f608143eff0b30dabb8b
viprasharma <54271021+viprasharma@users.noreply.github.com>
2020-05-14 20:02:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var hypnoticBall, database;
+var position;
+
+
+function setup(){
+  // to initiate the firebase database
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  hypnoticBall = createSprite(250,250,10,10);
+  hypnoticBall.shapeColor = "red";
+
+// fetching the position for hypnotic ball from database
+  var hypnoticBallPosition = database.ref
+  ('ball/position');
+  hypnoticBallPosition.on
+  ("value", readPosition, showError);
+}
+
+function draw(){
+  background("white");
+  if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+  
+}
+}
+function writePosition(x,y){
+  
+  //". set" indicates setting the the position in the database
+
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  console.log(position.x);
+  hypnoticBall.x = position.x;
+  hypnoticBall.y = position.y;
+  console.log("hello");
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
